ef32335b6481b566247ba6ac0e9d12503f75662d
Miika Ruissalo <caliba@gmail.com>
2015-03-25 17:05:55 +0200

    hakupalkki ja routeja

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,13 @@ Rails.application.routes.draw do
 
   get 'buildings/edit'
 
-  get 'pages/index'
+  resources :buildings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'buildings#index'
-
-  get 'pages/search'
-
-  get 'places/show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
